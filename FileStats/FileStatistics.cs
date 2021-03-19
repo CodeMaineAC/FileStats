719817cb3029ea26316e9c53d5e63f5f523b67f4
Benjamin Goff <bgoff@mainerobotics.org>
2021-03-19 09:06:13 -0400

    Add project files.

---
+++ FileStats/FileStatistics.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace FileStats
+{
+    class FileStatistics
+    {
+        static void Main(string[] args)
+        {
+
+            string fileName;
+            Console.WriteLine("HEnter a file name>> ");
+            fileName = Console.ReadLine();
+
+           
+
+
+            if(File.Exists(fileName))
+            {
+                Console.WriteLine("File exists");
+                Console.WriteLine("File was created {0}" +
+                    "\nFile was last written to {1}", 
+                    File.GetCreationTime(fileName),
+                    File.GetLastWriteTime(fileName));
+
+            }
+            else
+            {
+                Console.WriteLine("FILE DOES NOT EXIST");
+            }
+        }
+    }
+}
